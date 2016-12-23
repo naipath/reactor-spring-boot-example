232edce7656b7a111cb1bfb676c68dd55e1e1a24
Teije van Sloten <teije.van.sloten@hva.nl>
2016-12-23 10:36:37 +0100

    Initial setup of a spring boot reactor project

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Reactor-Spring-Boot-Example
+
+This project is meant to demo some of the functionality of Reactor
+ 
+You can run this project with:
+```bash
+mvn clean package spring-boot:run
+```
+
+To see results:
+```bash
+curl localhost:8080/test/somestringfortesting
+```
---
+++ src/main/java/nl/ordina/TestController.java
@@ -0,0 +1,15 @@
+package nl.ordina;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test/{id}")
+    public Flux<Integer> testing(@PathVariable String id) {
+        return Flux.just(id).map(String::length);
+    }
+}
