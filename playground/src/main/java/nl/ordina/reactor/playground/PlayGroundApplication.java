917460ba47b237bc42a2a1238a057e691d156122
tvs <teije-vs@hotmail.com>
2017-03-30 13:53:58 +0200

    Removed unused commit

--- playground/src/main/java/nl/ordina/reactor/playground/PlayGroundApplication.java
+++ playground/src/main/java/nl/ordina/reactor/playground/PlayGroundApplication.java
@@ -2,7 +2,6 @@ package nl.ordina.reactor.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class PlayGroundApplication {
