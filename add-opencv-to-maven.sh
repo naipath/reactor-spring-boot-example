376430c6ec83033882052e3bf48b6047dcea31bc
tvs <teije-vs@hotmail.com>
2017-03-30 10:04:03 +0200

    Moved face recognition in a seperate dir

--- add-opencv-to-maven.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mvn install:install-file \
-   -Dfile=/usr/local/Cellar/opencv3/3.2.0/share/OpenCV/java/opencv-320.jar \
-   -DgroupId=org.opencv \
-   -DartifactId=opencv \
-   -Dversion=3.2.0 \
-   -Dpackaging=jar \
-   -DgeneratePom=true
-
