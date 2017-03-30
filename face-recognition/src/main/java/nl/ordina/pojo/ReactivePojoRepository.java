df48d88f524645f05746763e07f56a4b5dc775d8
tvs <teije-vs@hotmail.com>
2017-03-30 11:39:01 +0200

    Removed unused Testing classes

--- face-recognition/src/main/java/nl/ordina/pojo/ReactivePojoRepository.java
+++
@@ -1,5 +0,0 @@
-package nl.ordina.pojo;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-interface ReactivePojoRepository extends ReactiveCrudRepository<Pojo, String> {}
